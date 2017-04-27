2cfc69470ea1fe3591870b46ebbf9b70912e63f4
Liz Vanderkloot <liz@lizvdk.com>
2017-04-27 10:24:38 -0400

    Alexa replies with current alert level

--- lib/french_toast_server/web/controllers/alexa_controller.ex
+++ lib/french_toast_server/web/controllers/alexa_controller.ex
@@ -2,20 +2,23 @@ defmodule FrenchToastServer.Web.AlexaController do
   use FrenchToastServer.Web, :controller
   use PhoenixAlexa.Controller, :post
 
-  def launch_request(conn, request) do
+  alias FrenchToastServer.Data
+
+  def launch_request(conn, _request) do
     response = %Response{}
-        |> set_output_speech(%TextOutputSpeech{text: "Welcome to the Horoscope."})
+        |> set_output_speech(%TextOutputSpeech{text: "Welcome to Hello Toast."})
 
     conn
       |> set_response(response)
   end
 
-  def session_end_request(conn, request) do
+  def session_end_request(conn, _request) do
     conn
   end
 
-  def intent_request(conn, "GetHelloToastIntent", request) do
-    text = "According to UniversalHub, the current French Toast Alert Level is severe."
+  def intent_request(conn, "GetHelloToastIntent", _request) do
+    current_status_level = Data.get_current_status.data_level.name
+    text = "According to UniversalHub, the current French Toast Alert Level is #{current_status_level}."
         response = %Response{}
                     |> set_output_speech(%TextOutputSpeech{text: text})
                     |> set_should_end_session(true)
