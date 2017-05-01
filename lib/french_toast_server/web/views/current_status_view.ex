b733c7f879636215184e7c9509103bcda8d9feb3
Liz Vanderkloot <liz@lizvdk.com>
2017-05-01 15:29:46 -0400

    Add description to current status

--- lib/french_toast_server/web/views/current_status_view.ex
+++ lib/french_toast_server/web/views/current_status_view.ex
@@ -3,6 +3,6 @@ defmodule FrenchToastServer.Web.CurrentStatusView do
   alias FrenchToastServer.Web.CurrentStatusView
 
   def render("show.json", %{status: status}) do
-    %{id: status.id, created_at: status.inserted_at, level: status.data_level.name}
+    %{id: status.id, created_at: status.inserted_at, level: status.data_level.name, description: status.data_level.description}
   end
 end
